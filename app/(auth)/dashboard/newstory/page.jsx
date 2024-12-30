6a9ff0bbec1329efa1363b49ae2b259684e43f64
Thirunavukarasan8489 <thirugopi733@gmail.com>
2024-12-30 12:50:52 +0530

    testing

--- app/(auth)/dashboard/newstory/page.jsx
+++ app/(auth)/dashboard/newstory/page.jsx
@@ -1,11 +1,16 @@
 "use client";
-import Editor from "./editor";
+// import Editor from "./editor";
+import dynamic from "next/dynamic";
 import ProtectedRoute from "../protectedroute";
+// Dynamically import your EditorJS component
+const EditorComponent = dynamic(() => import("./editor"), {
+  ssr: false, // Disable SSR for this component
+});
 export default function App() {
   return (
     <div>
       <ProtectedRoute>
-        <Editor editorBlock="editorjs-container" />
+        <EditorComponent editorBlock="editorjs-container" />
       </ProtectedRoute>
     </div>
   );
