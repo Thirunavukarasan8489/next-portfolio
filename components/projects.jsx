b69c16c1a4c0cbc3655c32cbb5f6d3f3550e1837
Thirunavukarasan8489 <thirugopi733@gmail.com>
2026-05-27 17:05:54 +0530

    projects updated

--- components/projects.jsx
+++ components/projects.jsx
@@ -2,54 +2,111 @@
 import { useState } from "react";
 import MNCVEL from "@/public/images/projects/MNC Vel.jpg";
 import RVSYUVA from "@/public/images/projects/rvsyuvalogo.png";
-import MantelCorp from "@/public/images/projects/mantel corp.png";
-import Rithans from "@/public/images/projects/rithans.png";
+// import MantelCorp from "@/public/images/projects/mantel corp.png";
+// import Rithans from "@/public/images/projects/rithans.png";
+import Dashboard from "@/public/images/projects/dashboard.png";
+import Transformtechin from "@/public/images/projects/transformtechin.png";
+import Transformtechai from "@/public/images/projects/transformtechai.png";
+import Harish from "@/public/images/projects/harish.png";
+import Creative from "@/public/images/projects/creative.png";
+import IasPipp from "@/public/images/projects/ias-pipp.png";
+import Truegut from "@/public/images/projects/truegut.png";
 import Pipp from "@/public/images/projects/pipp.png";
 import Image from "next/image";
+import Link from "next/link";
 export default function Projects() {
   // Sample data for the portfolio items
   const items = [
     {
       id: 1,
-      title: "MNC Vel Kalyana Mandapam",
-      category: "Website",
-      image: MNCVEL,
-      link: "https://www.mncvel.com/",
-    },
-    {
-      id: 2,
       title: "RVS YUVA",
       category: "Website",
       image: RVSYUVA,
       link: "https://rvsyuva.org/",
     },
     {
-      id: 3,
-      title: "Mnatel Corp Ltd",
-      category: "Website",
-      image: MantelCorp,
-      link: "https://mantelcorp.co.uk/",
-    },
-    {
-      id: 4,
-      title: "Rithans Arusuvai Catering Service",
-      category: "Website",
-      image: Rithans,
-      link: "https://rithans.rvscas.ac.in/",
-    },
-    {
-      id: 5,
+      id: 2,
       title: "Padmavathi Institute of Public Policy",
       category: "Website",
       image: Pipp,
-      link: "https://pipp.rvscas.ac.in/",
+      link: "https://pipp.org.in/",
+    },
+    {
+      id: 3,
+      title: "RVS Institute Database Dashboard",
+      category: "Web Application",
+      image: Dashboard,
+      link: "https://dashboard-rvscas.vercel.app/login",
+    },
+    {
+      id: 4,
+      title: "Padmavathi IAS Academy (PIA)",
+      category: "Website",
+      image: IasPipp,
+      link: "https://ias.pipp.org.in/",
+    },
+    {
+      id: 5,
+      title: "Truegut",
+      category: "E-commerce Website",
+      image: Truegut,
+      link: "https://truegut.com/",
+    },
+    {
+      id: 6,
+      title: "transformtech.in",
+      category: "Website",
+      image: Transformtechin,
+      link: "https://transformtech.in/",
+    },
+    {
+      id: 7,
+      title: "transformtech.ai",
+      category: "Website",
+      image: Transformtechai,
+      link: "https://transformtech.ai/",
+    },
+    {
+      id: 8,
+      title: "Harish Photography",
+      category: "Portfolio Website",
+      image: Harish,
+      link: "https://harish-portfolio-jade.vercel.app/",
+    },
+    {
+      id: 9,
+      title: "Creative Designs",
+      category: "Business Website",
+      image: Creative,
+      link: "https://creativelinegraphics.com/",
     },
     // {
-    //   id: 6,
+    //   id: 10,
     //   title: "",
     //   category: "",
     //   image: Pipp,
     //   link: "",
+    // },
+    // {
+    //   id: 11,
+    //   title: "",
+    //   category: "",
+    //   image: Pipp,
+    //   link: "",
+    // },
+    // {
+    //   id: 12,
+    //   title: "",
+    //   category: "",
+    //   image: Pipp,
+    //   link: "",
+    // },
+    // {
+    //   id: 1,
+    //   title: "MNC Vel Kalyana Mandapam",
+    //   category: "Website",
+    //   image: MNCVEL,
+    //   link: "https://www.mncvel.com/",
     // },
   ];
   // Tabs for filtering
@@ -89,11 +146,13 @@ export default function Projects() {
                   key={item.id}
                   className="bg-white shadow-md group rounded-lg overflow-hidden transition hover:scale-105 hover:shadow-2xl"
                 >
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
-                  />
+                  <Link href={item.link} target="_blank">
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      className="w-full h-48 object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
+                    />
+                  </Link>
                   <div className="p-4 text-center border-t-2 border-primary">
                     <h3 className="text-lg font-semibold group-hover:underline underline-offset-2 decoration-primary">
                       <a href={item.link} target="_blank">
